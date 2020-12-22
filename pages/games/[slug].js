641fef99acb366a7ecc2577b8d7cdc3d23c463e9
Tudor <sekaru@sleepystudios.net>
2020-12-22 20:12:34 +0000

    Revert "use canonical url for og:image in game"
    This reverts commit 7da18454568353fb3805bf15013eb2d5e0213fb1.

--- pages/games/[slug].js
+++ pages/games/[slug].js
@@ -12,7 +12,7 @@ const Game = ({ game }) => {
         <title>Sleepy Studios - {game.name}</title>
         <meta property='og:title' content={`${game.name} by Sleepy Studios`} key='og:title' />
         <meta property='og:description' content={game.desc} key='og:description' />
-        <meta property='og:image' content={`https://sleepystudios.net/_next/image?url=%2Fimages%2Fgames%${game.poster}&w=1920&q=75`} key='og:image' />
+        <meta property='og:image' content={`/images/games/${game.poster}`} key='og:image' />
       </Head>
 
       <motion.div
