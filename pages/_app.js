310f69fac9e6dfc3b42e474b0b8f1b4cde89d094
Tudor <sekaru@sleepystudios.net>
2020-12-23 22:44:28 +0000

    Revert "test - fix utm not tracking?"
    This reverts commit e4a3d13672baf1f2e00ab5f13900984ad72fffc2.

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,12 @@ import '../styles/styles.scss'
 import Nav from '../components/Nav'
 import Credit from '../components/Credit'
 import { AnimatePresence } from 'framer-motion'
-import Head from 'next/head'
 
 function App({ Component, pageProps, router }) {
   const isError = router.route === '/_error'
   
   return (
     <>
-      <Head>
-        <script async defer data-domain='sleepystudios.net' src='https://stats.sleepystudios.net/js/plausible.js' />
-      </Head>
-
       {!isError && <Nav />}
 
       <AnimatePresence exitBeforeEnter>
