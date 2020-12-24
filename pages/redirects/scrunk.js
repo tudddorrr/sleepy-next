33fe9f3c656f5e96c71994f44099c365934c066b
Tudor <sekaru@sleepystudios.net>
2020-12-24 12:03:58 +0000

    update utm

--- pages/redirects/scrunk.js
+++ pages/redirects/scrunk.js
@@ -1,2 +1,2 @@
 import redirect from 'nextjs-redirect'
-export default redirect('https://scrunk.sleepystudios.net/?utm_source=sleepy-redirect')
+export default redirect('https://scrunk.sleepystudios.net?utm_source=sleepy+redirect')
