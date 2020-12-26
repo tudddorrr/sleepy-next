319835a27258388464fdbd9247539c4145ca069e
Tudor <sekaru@sleepystudios.net>
2020-12-26 23:58:30 +0000

    fix accessibility issues

--- components/Credit.js
+++ components/Credit.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Credit = () => (
   <div id='credit'>
     <a href='https://malicoxon.co.uk' target='_blank' rel='noopener' aria-label='Website designed by Mali Coxon'>
-      <Image width='80' height='53' src='/images/icon_with_padding_invert.png' />
+      <Image width='80' height='53' src='/images/icon_with_padding_invert.png' alt='' />
     </a>
     <p>WEBSITE DESIGNED BY MALI COXON</p>
   </div>
